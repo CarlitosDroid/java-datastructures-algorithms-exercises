a11bd3048b688cc6777d013b23b418b531c324c9
Carlos Vargas Huaman <cvargas@godaddy.com>
2023-05-27 13:11:38 -0500

    Added exercise

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "abc";
+        reverseString(name.toCharArray());
+    }
+
+    /**
+     * Time Complexity = O(n)
+     * Space Complexity = O(1)
+     */
+    private static void reverseString(char[] array) {
+        int i = 0;
+        int j = array.length - 1;
+
+        while(i < j) {
+            // swap
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+
+        System.out.println("The reverse string is " + String.valueOf(array));
     }
 }
