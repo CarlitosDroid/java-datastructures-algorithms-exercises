8186feb8d0c17ca0370995aca45a9c14d4473de0
Carlos Vargas Huaman <cvargas@godaddy.com>
2023-05-29 20:41:32 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "caarlos";
+        sortString(str);
     }
+
+    /**
+     * Time Complexity = O(MAX_LETTERS*n) => O(n)
+     * Space complexity = O(1)
+     * @param str
+     */
+    private static void sortString(String str) {
+        int[] letters = new int[MAX_LETTERS];
+
+        // count the letters in the string
+        for (int i = 0; i < str.length(); i++) {
+            int position = str.charAt(i) - 'a';
+            letters[position]++;
+        }
+
+        for (int i = 0; i < MAX_LETTERS; i++) {
+            for (int j = 0; j < letters[i]; j++) {
+                System.out.print(((char) ('a' + i)));
+            }
+        }
+    }
+
+    // This represents the quantity of letters in the alphabet(`a` - `z`)
+    private static final int MAX_LETTERS = 26;
 }
