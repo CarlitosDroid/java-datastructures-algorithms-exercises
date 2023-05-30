7ae069cbad65ae5c502bb0b67d19d2083d4bf6ed
Carlos Vargas Huaman <cvargas@godaddy.com>
2023-05-29 22:00:09 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "aaabbbccd";
+
+        printFrequencyOfLetters(name);
+        System.out.println("----------------------------");
+        System.out.println("Second Way: Using Hash Table");
+        System.out.println("----------------------------");
+        printFrequencyOfLetters2(name);
+
+    }
+
+    /**
+     * Time Complexity O(n)
+     * Space Complexity O(1)
+     */
+    private static void printFrequencyOfLetters(String str) {
+        int[] frequency = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            int position = str.charAt(i) - 'a';
+            frequency[position]++;
+        }
+
+        for (int i = 0; i < frequency.length; i++) {
+            System.out.println("letter " + (char)('a' + i) + " quantity " +  frequency[i]);
+        }
+    }
+
+    /**
+     * Time Complexity O(n)
+     * Space Complexity O(n)
+     */
+    private static void printFrequencyOfLetters2(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(map.containsKey(str.charAt(i))) {
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            } else {
+                map.put(str.charAt(i), 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> mapEntry: map.entrySet()) {
+            System.out.println("letter " + mapEntry.getKey() + " quantity " + mapEntry.getValue());
+        }
+
     }
 }
