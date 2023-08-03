777b426986b6090afe3528d6fb2d2343390793b7
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-03 17:30:14 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "abba";
+        int n = str.length();
+        System.out.println(isPalindrome(str, n));
+    }
+
+    private static boolean isPalindrome(String str, int n) {
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 }
