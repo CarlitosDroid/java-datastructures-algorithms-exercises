90a6392ff2e0a6d985a479cc642d8980736f74e1
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-08 13:27:26 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int k = 4; // size of subarray
-        //System.out.println("Max sum of " + maximumSumOfSubArray(array, array.length, k));
-        System.out.println("Max sum of " + maximumSumOfSubArray1(array, array.length, k));
+        //System.out.println("Max sum of " + maxSubArraySum(array, array.length, k));
+        //System.out.println("Max sum of " + maxSubArraySum1(array, array.length, k));
+        System.out.println("Max sum of " + maxSubArraySum2(array, array.length));
     }
 
     /**
      * Naive approach, Brute Force Approach
      * Time Complexity: O(n*k)
      */
-    private static int maximumSumOfSubArray(int[] array, int n, int k) {
+    private static int maxSubArraySum(int[] array, int n, int k) {
         int max_sum = Integer.MIN_VALUE;
 
         for (int i = 0; i < n - k + 1; i++) {
@@ -28,15 +29,10 @@ public class Main {
     /**
      * Using Sliding Window
      * Time Complexity: O(n)
-     */
-
-    /**
      *
-     * Using Sliding Window
-     * Time Complexity: O(n)
      * @param k The size of subarray
      */
-    private static int maximumSumOfSubArray1(int[] array, int n, int k) { // Sliding Window Technique
+    private static int maxSubArraySum1(int[] array, int n, int k) { // Sliding Window Technique
 
         int maxSum = 0;
         for (int i = 0; i < k; i++) {
@@ -52,4 +48,29 @@ public class Main {
 
         return maxSum;
     }
+
+    /**
+     * Using Kadane's Algorithm
+     * Time Complexity: O(n)
+     *
+     */
+    static int maxSubArraySum2(int[] array, int n) { // Kadane's Algorithm
+        int max_so_far = Integer.MIN_VALUE;
+        int max_ending_here = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            max_ending_here += array[i];
+
+            if (max_so_far < max_ending_here){
+                max_so_far = max_ending_here;
+            }
+
+            if (max_ending_here < 0){
+                max_ending_here = 0;
+            }
+        }
+        return max_so_far;
+    }
+
 }
