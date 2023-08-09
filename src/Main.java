4b6c874b0e2f3ce60f0b6d888b036da9d4c1b4ed
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-09 14:19:59 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,39 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{6, -3, -10, 0, 2};
         int n = array.length;
-        resolve(array, n);
-        maxSubarrayProduct(array);
+        //maxSubarrayProduct(array, n);
+        maxSubarrayProduct2(array, n);
     }
 
-    static void maxSubarrayProduct(int arr[]) {
-        // Initializing result
-        int result = arr[0];
-        int n = arr.length;
-
+    static void maxSubarrayProduct(int[] array, int n) {
+        int maxProduct = 0;
         for (int i = 0; i < n; i++) {
-            int mul = arr[i];
-            // traversing in current subarray
+            int mul = array[i];
             for (int j = i + 1; j < n; j++) {
-                // updating result every time to keep an eye
-                // over the maximum product
-                result = Math.max(result, mul);
-                mul *= arr[j];
+                maxProduct = Math.max(maxProduct, mul);
+                mul *= array[j];
             }
-            // updating the result for (n-1)th index.
-            result = Math.max(result, mul);
+            maxProduct = Math.max(maxProduct, mul);
         }
-        System.out.println("maxium product is " + result);
+        System.out.println("maximum product is " + maxProduct);
     }
 
     /**
      * Kadane's algorithm
      */
-    private static void resolve(int[] array, int n) {
-        int max_ending_here = array[0];
-        int min_ending_here = array[0];
-        int max_so_far = array[0];
+    private static void maxSubarrayProduct2(int[] array, int n) {
+        int maxSoFar = array[0]; // highest product
+        int minSoFar = array[0]; // lowest product
+        int result = maxSoFar;
 
         for (int i = 1; i < n; i++) {
-            int temp = Math.max(
-                    Math.max(array[i], array[i] * max_ending_here),
-                    array[i] * min_ending_here);
-            min_ending_here = Math.min(
-                    Math.min(array[i], array[i] * max_ending_here),
-                    array[i] * min_ending_here);
-            max_ending_here = temp;
-            max_so_far = Math.max(max_so_far, max_ending_here);
+            int curr = array[i];
+            int tempMax = Math.max(curr, Math.max(maxSoFar * curr, minSoFar * curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar * curr, minSoFar * curr));
+            maxSoFar = tempMax;
+            result = Math.max(maxSoFar, result);
         }
-        System.out.println("maxium product is " + max_so_far);
+        System.out.println("maximum product is " + result);
     }
 
 }
