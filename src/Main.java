f034b48c3fdc80a256c06892613d45b2a2820c8c
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-09 14:04:28 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,25 @@ public class Main {
         int[] array = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int n = array.length;
 
-        System.out.println("The maximum sum is " + maxSubArraySum2(array, n));
+        System.out.println("The maximum sum is " + maxSubArraySum1(array, n));
+        //System.out.println("The maximum sum is " + maxSubArraySum2(array, n));
+    }
+
+
+    private static int maxSubArraySum1(int[] array, int n) { // Brute Force
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = array[i];
+            for (int j = i + 1; j < n; j++) {
+                maxSum = Math.max(maxSum, number);
+                number += array[j];
+            }
+            maxSum = Math.max(maxSum, number);
+        }
+
+        return maxSum;
     }
 
 
