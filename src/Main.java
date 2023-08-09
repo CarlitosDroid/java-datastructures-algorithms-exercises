4e7dffdd5d04fc57d42d608703a77b22df6452f5
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-09 13:40:28 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = array.length;
+
+        System.out.println("The maximum sum is " + maxSubArraySum2(array, n));
     }
+
+
+    /**
+     * Using Kadane's Algorithm
+     * Time Complexity: O(n)
+     *
+     */
+    static int maxSubArraySum2(int[] array, int n) { // Kadane's Algorithm
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndingHere = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxEndingHere += array[i];
+            maxEndingHere = Math.max(maxEndingHere, 0);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+
 }
