7b26ebc9ee6941e8576f90d5daad7910fd94a3fe
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-12 12:01:52 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        areBracketsBalanced("{()}[]");
+    }
+
+    private static void areBracketsBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    System.out.println("Invalid parenthesis");
+                    return;
+                } else {
+                    char top = stack.peek();
+                    if (top == '(' && c == ')' ||
+                            top == '[' && c == ']' ||
+                            top == '{' && c == '}') {
+                        stack.pop();
+                    } else {
+                        System.out.println("Invalid parenthesis");
+                    }
+                }
+            }
+        }
+
+        if(stack.isEmpty()) {
+            System.out.println("IS VALID");
+        } else {
+            System.out.println("IS NOT VALID");
+        }
     }
 }
