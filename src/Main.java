3b926a00b09ef8e982f2057aee1a27bb6567f07b
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-12 01:06:49 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "ABCD";
+        rotate(str, str.length());
+    }
+
+    private static void rotate(String str, int n) {
+        for (int i = 0; i < n; i++) {
+            str = str.substring(1) + str.charAt(0);
+            System.out.println(str);
+        }
     }
 }
