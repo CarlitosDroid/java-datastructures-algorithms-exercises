4114c2f2373aa977f8831db21631378566dbef81
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-12 18:36:17 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "salritoc";
+        int[] array = new int[]{7, 1, 3, 2, 4, 5, 6, 0};
+        sortMapByKey(s, array);;
+    }
+
+    private static void sortMapByKey(String s, int[] array) {
+        Map<Character, Integer> unsortedMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            unsortedMap.put(s.charAt(i), array[i]);
+        }
+
+        Map<Character, Integer> treeMap = new TreeMap<>(Comparator.naturalOrder()); // or reverseOrder
+        treeMap.putAll(unsortedMap);
+        System.out.println(treeMap);
     }
 }
