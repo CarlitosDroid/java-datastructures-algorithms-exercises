a45425fab0a5db74b0f7c4e3ea699aef9c448838
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-12 13:28:29 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int N = 12345;
+        reverseNumber(N);
+    }
+
+    private static void reverseNumber(int N) {
+        int reverse = 0;
+        int r;
+
+        while (N > 0) {
+            r = N % 10;
+            reverse = reverse * 10 + r;
+            N = N / 10;
+        }
+
+        System.out.println("The reverse number is " + reverse);
     }
 }
