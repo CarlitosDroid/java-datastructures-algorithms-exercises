7ab28220831c06d369be190df26e134aaf008aed
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-12 23:47:00 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "DROIDDROD";
+        findFirstNonRepeatingCharacter(str);
+    }
+
+    private static void findFirstNonRepeatingCharacter(String str) {
+
+        if (str.isEmpty()) {
+            System.out.println("Empty string");
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+                System.out.println("The element is " + str.charAt(i));
+                break;
+            }
+        }
     }
 }
