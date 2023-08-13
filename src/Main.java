6a0a8aa341939fa718a3416565894de9d46cfee5
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-13 00:45:16 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(0);
+        int res = 0;
+
+        while (head != null) {
+            res = res * 2 + head.val;
+            head = head.next;
+        }
+
+        System.out.println("The answer is " + res);
     }
 }
