54cbae4753706dc11e2c164d7c6239d249e89ace
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-08-15 13:25:47 -0500

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "AB";
+        String str2 = "CD";
+
+        interleavings(str1, 0, str2, 0, "");
+    }
+
+    private static void interleavings(String str1, int i, String str2, int j, String result) { // iterative
+
+        if (i == str1.length() && j == str2.length()) {
+            System.out.println(result);
+        }
+
+        if (i < str1.length()) {
+            interleavings(str1, i + 1, str2, j, result + str1.charAt(i));
+        }
+
+        if (j < str2.length()) {
+            interleavings(str1, i, str2, j + 1, result + str2.charAt(j));
+        }
     }
 }
