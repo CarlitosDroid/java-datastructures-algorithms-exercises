ad7cc476713a509265685f2c22227cd73b8f72b3
CLVargas <cl.vargas@globant.com>
2024-03-27 11:48:14 -0500

    Updated comment

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = new int[]{10, 2, 15, 20, 19, 90, 67};
-        System.out.println("The element is " + findElementHigherThanNeighbours(array));
+        System.out.println("The element is " + findFirstElementHigherThanNeighbours(array));
     }
 
     /**
      * Time Complexity: O(n)
-     *
+     * Only compare current element with left and right elements
      */
-    private static int findElementHigherThanNeighbours(int[] array) {
+    private static int findFirstElementHigherThanNeighbours(int[] array) {
         for (int i = 1; i < array.length - 1; i++) {
             if (array[i] > array[i-1] && array[i] > array[i+1]) {
                 return array[i];
